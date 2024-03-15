e0613a2b5a29288c79957c9e1dee205e2b245196
abdoforda <abdelrahmaan3@gmail.com>
2024-03-15 07:52:28 +0200

    a

---
+++ resources/views/pages/forex.blade.php
@@ -0,0 +1,32 @@
+<div class="container pt-5 pb-2">
+    <div class="row g-4">
+        <div class="col-md-12">
+            <h3>@lang('Experience the Forex market with Mercato Brokers.')</h3>
+            <p>@lang("Join the global phenomenon of Forex trading with Mercato Brokers. Whether you're passionate
+                about economics or keen on staying informed about monetary policies, employment trends, or
+                inflation rates, our Forex platform offers a comprehensive range of Major, Minor, and Exotic
+                currency pairs to suit your trading preferences.")</p>
+                </div>
+        <div class="col-md-12 mt-3">
+            <h3>@lang('Discover the advantages of Forex trading:')</h3>
+            <ul class="list-style-inside">
+                <li>@lang("Dive into the world's largest financial market, where currencies are traded around the clock,
+                    providing ample opportunities for profit potential.")</li>
+                <li>@lang("Engage in dynamic trading sessions, responding to real-time market events and news that
+                    impact currency values.")</li>
+                <li>@lang("Enjoy high liquidity, allowing for seamless execution of trades with minimal slippage, even for
+                    large positions.")</li>
+                <li>@lang("Access leverage, amplifying your trading capital to potentially enhance profits (note: leverage
+                    can also magnify losses, so it should be used cautiously).")</li>
+                <li>@lang("Benefit from the ability to go long or short on currency pairs, enabling you to profit from both
+                    rising and falling markets.")</li>
+                <li>@lang("Trade a diverse range of currency pairs, including Majors, Minors, and Exotics, catering to
+                    various trading strategies and risk appetites.")</li>
+                <li>@lang("Utilize advanced trading tools and analysis techniques to identify trends, patterns, and
+                    entry/exit points, empowering you to make informed trading decisions.")</li>
+                <li>@lang("Enjoy flexibility in trading hours, as the Forex market operates 24 hours a day, five days a
+                    week, allowing you to trade whenever suits your schedule.")</li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/mt5.blade.php
@@ -0,0 +1,108 @@
+<div class="container pt-5 pb-2">
+    <div class="row g-4 text-center">
+        <div class="col-md-12 mb-4">
+            <h3 class="h1 color-primary">
+                @lang('MT5 upgrades')
+            </h3>
+        </div>
+        <div class="col-md-3">
+            <h3>@lang('Faster electronic trading.')</h3>
+            <p>@lang('64-bit, multi-threaded platform for faster and more efficient trading than MT4.')</p>
+        </div>
+        <div class="col-md-3">
+            <h3>@lang('Simpler search function ')</h3>
+            <p>@lang('Type a market name, find it instantly, and avoid scrolling.')</p>
+        </div>
+        <div class="col-md-3">
+            <h3>@lang('Improved range of tools')</h3>
+            <p>@lang('Access 15 custom indicators, such as integrated Pivot Points, plus an array of advanced charting tools.')</p>
+        </div>
+        <div class="col-md-3">
+            <h3>@lang('Choose from nearly 300 stocks')</h3>
+            <p>@lang('Diversify your trading with a range of CFD stocks, previously unavailable in MT4.')</p>
+        </div>
+        <div class="col-md-12 mt-4">
+            <h3>@lang('Exclusive tools')</h3>
+            <p class="lead">@lang('Optimize your strategy with a suite of over 20 tools that give you professional-grade control and flexibility over your trading.')</p>
+        </div>
+    </div>
+</div>
+
+<div class="pt-5 pb-5 bg-gray">
+    <div class="container">
+        <div class="row justify-content-center align-items-center g-2">
+            <div class="col-md-12">
+
+            </div>
+            <div class="col-md-6">
+                <img class="rounded img-fluid" src="{{ asset('img/website/get_mt5.jpg') }}" alt="Get more from MT5">
+            </div>
+            <div class="col-md-6">
+                <h3 class="color-primary h2">@lang('Get more from MT5')</h3>
+                <p>
+                    @lang('Take advantage of more Expert Advisors, more indicators, and a whole suite of additional apps.')
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="bg-white">
+    <div class="container pt-5 pb-4">
+        <div class="row g-2 text-center">
+            <div class="col-md-12 mb-4">
+                <h3 class="h1 color-primary">
+                    @lang('Additional perks')
+                </h3>
+            </div>
+            <div class="col-md-4">
+                <h3>@lang('Integrated Bloomberg news')</h3>
+                <p>@lang("Unlike many trading platforms, you'll enjoy access to integrated Bloomberg news.")</p>
+            </div>
+            <div class="col-md-4">
+                <h3>@lang('Smooth operation')</h3>
+                <p>@lang("Leverage the platform's functionalities seamlessly and without relying on external connections.")</p>
+            </div>
+            <div class="col-md-4">
+                <h3>@lang('Algorithmic trading with EAs')</h3>
+                <p>@lang('Craft your custom algorithms within a tailored environment.')</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="pt-5 pb-5">
+    <div class="container">
+        <div class="row justify-content-center align-items-center g-0">
+            <div class="col-md-6">
+                <h3 class="color-primary h2">@lang('Desktop MetaTrader 5')</h3>
+                <p class="lead">
+                    @lang("Unlock the full MetaTrader 5 package. It boasts an extensive array of tools and indicators and serves as the exclusive gateway to Expert Advisors.")
+                </p>
+                <div>
+                    <h4>@lang('Expert Advisors')</h4>
+                    <p>@lang("Harness the power of trading algorithms and comprehensive development and testing environments.")</p>
+                </div>
+                <hr>
+                <div>
+                    <h4>@lang("MetaTrader Market")</h4>
+                    <p>@lang("Choose from a selection of more than 950 trading robots and 2,000 technical indicators available in the largest trading marketplace globally.")</p>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-lg btn-primary2">
+                        Download
+                    </button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img class="rounded img-fluid" src="{{ asset('img/website/get_mt5.jpg') }}" alt="Get more from MT5">
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+
+
+
+
