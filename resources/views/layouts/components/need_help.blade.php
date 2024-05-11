7aa489481822a0440c2b8a62523730e66e64d569
abdoforda <abdelrahmaan3@gmail.com>
2024-05-11 16:45:39 +0300

    a

--- resources/views/layouts/components/need_help.blade.php
+++ resources/views/layouts/components/need_help.blade.php
@@ -1,33 +1,45 @@
-<section style="    background: #c5860329;">
-    <div class="container p-5">
-        <div class="row justify-content-center align-items-center g-4">
-            <div class="col">
-                <h1 class="text-center display-2" style="color: #08243c;">Need Help?</h1>
+<div
+    class="row  g-0 asdd"
+>
+    <div class="col0 col-md-8"><section style="    background: #c5860329;">
+        <div class="container p-5">
+            <div class="row justify-content-center align-items-center g-4">
+                <div class="col">
+                    <h1 class="text-center display-2" style="color: #08243c;">Need Help?</h1>
+                </div>
+                <div class="col">
+                    <center>
+                        <button type="button" class="btn btn-lg btn-primary"
+                            style="background: #e3b24c;
+                            color: #ffffff;
+                            border-color: #08243c05;
+                            font-size: 26px;
+                            cursor: pointer;">
+                            24/5 Support
+                        </button>
+                    </center>
+                </div>
             </div>
-            <div class="col">
+    
+        </div>
+    
+    </section></div>
+    <div class="col0 col-md-4"><section style="padding: 50px;
+        background: #e3b24c;">
+            <div class="container">
                 <center>
-                    <button type="button" class="btn btn-lg btn-primary"
-                        style="background: #e3b24c;
-                        color: #ffffff;
-                        border-color: #08243c05;
-                        font-size: 26px;
-                        cursor: pointer;">
-                        24/5 Support
-                    </button>
+                    <h2 class="text-white">Start trading Now</h2>
+                    <a href="/page/try-demo" class="btn-out01" >Demo Account</a>
                 </center>
             </div>
-        </div>
+        </section></div>
+</div>
 
-    </div>
 
-</section>
 
-<section style="padding: 24px;
-background: #e3b24c;">
-    <div class="container">
-        <center>
-            <h2 class="text-white">Start trading Now</h2>
-            <a href="/page/try-demo" class="btn-out01" >Demo Account</a>
-        </center>
-    </div>
-</section>
+
+<style>
+    .asdd .col0{
+        padding: 0 !important;
+    }
+</style>
