f66053732d1e02f95a465f19b02e2b835a74db0b
abdoforda <abdelrahmaan3@gmail.com>
2024-07-06 16:49:00 +0300

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="DIGITAL COIN Cryptocurrency Market and Trading Site Template">
-    <meta name="author" content="Ansonika">
-    <title>@yield('title')</title>
+    <meta name="description" content="Mercato Brokers">
+    <meta name="author" content="Mercato Brokers">
+    <title>@yield('title', 'Mercato Brokers')</title>
     <meta name="description" content="@yield('description')">
     <meta name="keywords" content="@yield('keywords')">
 
